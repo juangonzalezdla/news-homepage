3c9c3c1aa42a511fd47aa5c6c6e35c973b5e74dc
Juan Gonzalez <gonzalesjuandavid608@gmail.com>
2023-02-17 14:37:07 -0500

    Add mobile menu and opacity

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 const burgerMenu = document.getElementById('burger');
+const iconMenuOpen = document.querySelector('.navbar__hamburger__open');
+const iconMenuClose = document.querySelector('.navbar__hamburger__close');
+const menu = document.querySelector('.navbar__menu--mobile');
+const opacity = document.querySelector('.opacity');
 
-burgerMenu.addEventListener('click', () => {
-  burgerMenu.classList.toggle('change');
+const openMenu = () => {
+  iconMenuOpen.classList.add('inactive');
+  iconMenuClose.classList.remove('inactive');
+  opacity.classList.remove('inactive');
+  menu.style.width = '70%';
+}
+
+const closeMenu = () => {
+  iconMenuOpen.classList.remove('inactive');
+  iconMenuClose.classList.add('inactive');
+  opacity.classList.add('inactive');
+  menu.style.width = '0px';
+}
+
+iconMenuOpen.addEventListener('click', () => {
+  openMenu();
+});
+
+iconMenuClose.addEventListener('click', () => {
+  closeMenu();
 });
